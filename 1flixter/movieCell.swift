b547e8a28c003286afe70851977a23a61733078c
Jeury Tineo <jeurytineo@gmail.com>
2021-02-19 23:54:10 -0500

    version1

---
+++ 1flixter/movieCell.swift
@@ -0,0 +1,29 @@
+//
+//  movieCell.swift
+//  1flixter
+//
+//  Created by Jeury Tineo on 2/19/21.
+//
+
+import UIKit
+
+class movieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
